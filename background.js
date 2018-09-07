b1db4f540c9d7f97a1e43616d459c1bba1f864bb
Milan Donhowe <milanaugust@gmail.com>
2018-09-06 22:50:40 -0700

    Added file loading functionality
    idk why this works

--- background.js
+++ background.js
@@ -1,11 +1,13 @@
 chrome.runtime.onInstalled.addListener(function() {
 	console.log(' background.js loaded');
-	/*
-	if (window.File && window.FileReader && window.FileList && window.Blob) {
-		console.log('apis supported')
-	} else {
-		console.log('it no work')
-	}
-
-
+	let txt = '';
+	let xmlhttp = new XMLHttpRequest();
+	xmlhttp.onreadystatechange = function(){
+		if (xmlhttp.status == 200 && xmlhttp.readyState == 4){
+				txt = xmlhttp.responseText;
+				console.log(txt)
+			}
+	};
+	xmlhttp.open("GET", "period1.txt", true);
+	xmlhttp.send();
 });

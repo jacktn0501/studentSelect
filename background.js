5418a1483962b2ea37f6018301307c63b02869fb
Milan Donhowe <milanaugust@gmail.com>
2018-09-08 19:01:17 -0700

    Multiple File Loading Added

--- background.js
+++ background.js
@@ -1,21 +1,42 @@
 chrome.runtime.onInstalled.addListener(function() {
 	console.log(' background.js loaded');
-	let txt = '';
-	let xmlhttp = new XMLHttpRequest();
-	xmlhttp.onreadystatechange = function(){
-		if (xmlhttp.status == 200 && xmlhttp.readyState == 4){
+});
+
+
+// Listen for data request from detect.js
+
+chrome.extension.onConnect.addListener(function(port) {
+	
+	console.log("Connected... ")
+
+	port.onMessage.addListener(function(msg) {
+		console.log("message received " + msg);
+		
+		let txt = ''
+		let xmlhttp = new XMLHttpRequest();
+		
+		xmlhttp.onreadystatechange = function(){
+			if (xmlhttp.status == 200 && xmlhttp.readyState == 4){
+			
+				// Get text from file
 				txt = xmlhttp.responseText;
+				
+				// Get each line of file into an array
 				let stripped = txt.split('\n')
+				
+				// Remove empty lines and undefined lines
 				let studentNames = stripped.filter(slimDown);
+				
 				function slimDown(value){
 					return value != "" && value != undefined
 				}
 				
-				console.log(studentNames);
-				console.log(studentNames[2] === "");
-				console.log(studentNames[2])
+				port.postMessage(studentNames);
 			}
-	};
-	xmlhttp.open("GET", "period1.txt", true);
-	xmlhttp.send();
+		};
+		
+		xmlhttp.open("GET", msg + ".txt", true);
+		xmlhttp.send();
+		
+	});
 });

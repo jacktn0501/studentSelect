e36cb7acc30de3325479149f2823d5db517754f5
Milan Donhowe <milanaugust@gmail.com>
2018-09-11 09:43:42 -0700

    Change approach to chrome.storage

--- background.js
+++ background.js
@@ -2,6 +2,17 @@
 
 chrome.runtime.onInstalled.addListener(function() {
 	console.log(' background.js loaded');
+	
+	// set example class
+	
+	let periodExample = "period1";
+	let studentsExample = ["John", "Dave", "Anderson", "Rye", "Jane", "Elizabeth", "Mr. Darcy"];
+	chrome.storage.local.set({periodExample: studentsExample}, function(){
+		console.log('Students saved as ' + studentsExample)
+	});
+	chrome.storage.local.get({"period1": []}, function(data){
+		console.log(data.period1)
+	});
 });
 
 
@@ -14,6 +25,24 @@ chrome.extension.onConnect.addListener(function(port) {
 	port.onMessage.addListener(function(msg) {
 		console.log("message received " + msg);
 		
+		if (msg[0])
+		
+		let key = msg + ""
+		switch (msg){
+		
+			case "period1":
+				chrome.storage.local.get({"period1": []}, function(data){
+					console.log(data.period1)
+					port.postMessage(data.period1)
+				});
+				break;
+			
+			
+		}
+		
+		//port.postMessage(studentNames);
+		
+		/*
 		let txt = ''
 		let xmlhttp = new XMLHttpRequest();
 		
@@ -39,6 +68,7 @@ chrome.extension.onConnect.addListener(function(port) {
 		
 		xmlhttp.open("GET", msg + ".txt", true);
 		xmlhttp.send();
+		*/
 		
 	});
 });

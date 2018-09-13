d1d1196a98b21b619983ada49aee9af126fea254
Milan Donhowe <milanaugust@gmail.com>
2018-09-13 09:32:41 -0700

    Update background.js

--- background.js
+++ background.js
@@ -7,7 +7,7 @@ chrome.runtime.onInstalled.addListener(function() {
 	
 	let periodExample = "period1";
 	let studentsExample = ["John", "Dave", "Anderson", "Rye", "Jane", "Elizabeth", "Mr. Darcy"];
-	chrome.storage.local.set({periodExample: studentsExample}, function(){
+	chrome.storage.local.set({"period1": studentsExample}, function(){
 		console.log('Students saved as ' + studentsExample)
 	});
 	chrome.storage.local.get({"period1": []}, function(data){
@@ -23,52 +23,43 @@ chrome.extension.onConnect.addListener(function(port) {
 	console.log("Connected... ")
 
 	port.onMessage.addListener(function(msg) {
-		console.log("message received " + msg);
+		console.log("message received " + Array.isArray(msg));
 		
-		if (msg[0])
 		
-		let key = msg + ""
-		switch (msg){
+		if (Array.isArray(msg)){
+			
+			let test = msg[0];
+			
+			switch(test){
+				
+				case "period1":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period1": msg}, function(){
+						console.log(msg);
+					});
+					chrome.storage.local.get({"period1": []}, function(data){
+						console.log(data.period1)
+					});
+					break;
+				
+			}
+			
+		} else {
 		
-			case "period1":
-				chrome.storage.local.get({"period1": []}, function(data){
-					console.log(data.period1)
-					port.postMessage(data.period1)
-				});
-				break;
+			switch (msg){
+		
+				case "period1":
+					chrome.storage.local.get({"period1": []}, function(data){
+						console.log(data.period1)
+						port.postMessage(data.period1)
+					});
+					break;
 			
 			
+			}
 		}
 		
-		//port.postMessage(studentNames);
-		
-		/*
-		let txt = ''
-		let xmlhttp = new XMLHttpRequest();
-		
-		xmlhttp.onreadystatechange = function(){
-			if (xmlhttp.status == 200 && xmlhttp.readyState == 4){
-			
-				// Get text from file
-				txt = xmlhttp.responseText;
-				
-				// Get each line of file into an array
-				let stripped = txt.split('\n')
-				
-				// Remove empty lines and undefined lines
-				let studentNames = stripped.filter(slimDown);
-				
-				function slimDown(value){
-					return value != "" && value != undefined
-				}
-				
-				port.postMessage(studentNames);
-			}
-		};
-		
-		xmlhttp.open("GET", msg + ".txt", true);
-		xmlhttp.send();
-		*/
 		
 	});
 });

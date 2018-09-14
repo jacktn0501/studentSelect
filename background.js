dd8a5aace7085d7dc414ebf421fb273f6d171499
Milan Donhowe <milanaugust@gmail.com>
2018-09-13 17:59:48 -0700

    Add files via upload

--- background.js
+++ background.js
@@ -38,8 +38,63 @@ chrome.extension.onConnect.addListener(function(port) {
 					chrome.storage.local.set({"period1": msg}, function(){
 						console.log(msg);
 					});
-					chrome.storage.local.get({"period1": []}, function(data){
-						console.log(data.period1)
+					//chrome.storage.local.get({"period1": []}, function(data){
+					//	console.log(data.period1)
+					//});
+					break;
+				case "period2":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period2": msg}, function(){
+						console.log(msg);
+					});
+					break;
+					
+				case "period3":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period3": msg}, function(){
+						console.log(msg);
+					});
+					break;
+					
+				case "period4":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period4": msg}, function(){
+						console.log(msg);
+					});
+					break;
+					
+				case "period5":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period5": msg}, function(){
+						console.log(msg);
+					});
+					break;
+				
+				case "period6":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period6": msg}, function(){
+						console.log(msg);
+					});
+					break;
+					
+				case "period7":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period7": msg}, function(){
+						console.log(msg);
+					});
+					break;
+					
+				case "period8":
+					msg.shift();
+					console.log(msg);
+					chrome.storage.local.set({"period8": msg}, function(){
+						console.log(msg);
 					});
 					break;
 				
@@ -51,12 +106,52 @@ chrome.extension.onConnect.addListener(function(port) {
 		
 				case "period1":
 					chrome.storage.local.get({"period1": []}, function(data){
-						console.log(data.period1)
-						port.postMessage(data.period1)
+						console.log(data.period1);
+						port.postMessage(data.period1);
 					});
 					break;
-			
-			
+				case "period2":
+					chrome.storage.local.get({"period2": []}, function(data){
+						console.log(data.period2);
+						port.postMessage(data.period2);
+					});
+					break;
+				case "period3":
+					chrome.storage.local.get({"period3": []}, function(data){
+						console.log(data.period3);
+						port.postMessage(data.period3);
+					});
+					break;
+				case "period4":
+					chrome.storage.local.get({"period4": []}, function(data){
+						console.log(data.period4);
+						port.postMessage(data.period4);
+					});
+					break;
+				case "period5":
+					chrome.storage.local.get({"period5": []}, function(data){
+						console.log(data.period5);
+						port.postMessage(data.period5);
+					});
+					break;
+				case "period6":
+					chrome.storage.local.get({"period6": []}, function(data){
+						console.log(data.period6);
+						port.postMessage(data.period6);
+					});
+					break;
+				case "period7":
+					chrome.storage.local.get({"period7": []}, function(data){
+						console.log(data.period7);
+						port.postMessage(data.period7);
+					});
+					break;
+				case "period8":
+					chrome.storage.local.get({"period8": []}, function(data){
+						console.log(data.period8);
+						port.postMessage(data.period8);
+					});
+					break;
 			}
 		}
 		
